bcb2171c4b98575990be57d722ddb53988b71389
Nidhi16 <nidhi.valuable@gmail.com>
2017-10-31 23:49:48 +0530

    Created constructor function and made two objects out of them

---
+++ Assignment1.js
@@ -0,0 +1,22 @@
+// Constructor function
+function Car(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+
+    this.displayProperties = function() {
+        console.log("Make : " + this.make);
+        console.log("Model : " + this.model);
+        console.log("Year : " + this.year);
+    };
+}
+
+// creating objects
+var audiA8 = new Car('Volswagen', 'AudiA8', '1994');
+var audiQ7 = new Car('Audi AG', 'AudiQ7', '2005');
+
+// calling property
+console.log("Object1");
+audiA8.displayProperties();
+console.log("Object2");
+audiQ7.displayProperties();
